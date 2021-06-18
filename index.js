60d713b1da13a1cb9d06e3bc45713842ca9656c6
Andrew Suares <andrewlsuares@gmail.com>
2021-06-18 12:50:18 -0700

    Done

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+document.querySelector('#idea-btn').addEventListener('click', () => {
+	fetch('https://www.boredapi.com/api/activity')
+		.then(response => response.json())
+		.then(data => {
+			console.log(data)
+			document.querySelector('#activity').textContent = data.activity
+		})
+})
